7aa7f7f94c366c411fb7fd49aa7076696fefe8fa
zhangzhiwei <371452875@qq.com>
2019-09-12 18:05:33 +0800

    hello

---
+++ web/src/main/java/com/dozenx/web/module/storage/action/AdminStorageController.java
@@ -0,0 +1,85 @@
+package com.dozenx.web.module.storage.action;
+
+import com.dozenx.web.module.storage.bean.LitemallStorage;
+import com.dozenx.web.module.storage.service.LitemallStorageService;
+import com.dozenx.web.module.storage.service.StorageService;
+import com.dozenx.web.util.ResultUtil;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotNull;
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/storage")
+@Validated
+public class AdminStorageController {
+    private final Log logger = LogFactory.getLog(AdminStorageController.class);
+
+    @Autowired
+    private StorageService storageService;
+    @Autowired
+    private LitemallStorageService litemallStorageService;
+
+//    @RequiresPermissions("admin:storage:list")
+//    @RequiresPermissionsDesc(menu = {"系统管理", "对象存储"}, button = "查询")
+    @GetMapping("/list")
+    public Object list(String key, String name,
+                       @RequestParam(defaultValue = "1") Integer page,
+                       @RequestParam(defaultValue = "10") Integer limit,
+                       @RequestParam(defaultValue = "add_time") String sort,
+                        @RequestParam(defaultValue = "desc") String order) {
+        List<LitemallStorage> storageList = litemallStorageService.querySelective(key, name, page, limit, sort, order);
+        return ResultUtil.okList(storageList);
+    }
+
+//    @RequiresPermissions("admin:storage:create")
+//    @RequiresPermissionsDesc(menu = {"系统管理", "对象存储"}, button = "上传")
+    @PostMapping("/create")
+    public Object create(@RequestParam("file") MultipartFile file) throws IOException {
+        String originalFilename = file.getOriginalFilename();
+        LitemallStorage litemallStorage = storageService.store(file.getInputStream(), file.getSize(),
+                file.getContentType(), originalFilename);
+        return ResultUtil.ok(litemallStorage);
+    }
+
+//    @RequiresPermissions("admin:storage:read")
+//    @RequiresPermissionsDesc(menu = {"系统管理", "对象存储"}, button = "详情")
+    @PostMapping("/read")
+    public Object read(@NotNull Integer id) {
+        LitemallStorage storageInfo = litemallStorageService.findById(id);
+        if (storageInfo == null) {
+            return ResultUtil.badArgumentValue();
+        }
+        return ResultUtil.ok(storageInfo);
+    }
+
+//    @RequiresPermissions("admin:storage:update")
+//    @RequiresPermissionsDesc(menu = {"系统管理", "对象存储"}, button = "编辑")
+    @PostMapping("/update")
+    public Object update(@RequestBody LitemallStorage litemallStorage) {
+        if (litemallStorageService.update(litemallStorage) == 0) {
+            return ResultUtil.updatedDataFailed();
+        }
+        return ResultUtil.ok(litemallStorage);
+    }
+
+//    @RequiresPermissions("admin:storage:delete")
+//    @RequiresPermissionsDesc(menu = {"系统管理", "对象存储"}, button = "删除")
+    @PostMapping("/delete")
+    public Object delete(@RequestBody LitemallStorage litemallStorage) {
+        String key = litemallStorage.getKey();
+        if (StringUtils.isEmpty(key)) {
+            return ResultUtil.badArgument();
+        }
+        litemallStorageService.deleteByKey(key);
+        storageService.delete(key);
+        return ResultUtil.ok();
+    }
+}
---
+++ web/src/main/java/com/dozenx/web/module/storage/action/WxStorageController.java
@@ -0,0 +1,111 @@
+package com.dozenx.web.module.storage.action;
+
+import com.dozenx.common.util.StringUtil;
+import com.dozenx.web.module.storage.bean.LitemallStorage;
+import com.dozenx.web.module.storage.service.LitemallStorageService;
+import com.dozenx.web.module.storage.service.StorageService;
+import com.dozenx.web.util.ResultUtil;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * 对象存储服务
+ */
+@RestController
+@RequestMapping("/wx/storage")
+@Validated
+public class WxStorageController {
+    private final Log logger = LogFactory.getLog(WxStorageController.class);
+
+    @Autowired
+    private StorageService storageService;
+    @Autowired
+    private LitemallStorageService litemallStorageService;
+
+    private String generateKey(String originalFilename) {
+        int index = originalFilename.lastIndexOf('.');
+        String suffix = originalFilename.substring(index);
+
+        String key = null;
+        LitemallStorage storageInfo = null;
+
+        do {
+            key = StringUtil.getRandomString(20) + suffix;
+            storageInfo = litemallStorageService.findByKey(key);
+        }
+        while (storageInfo != null);
+
+        return key;
+    }
+
+    @PostMapping("/upload")
+    public Object upload(@RequestParam("file") MultipartFile file) throws IOException {
+        String originalFilename = file.getOriginalFilename();
+        LitemallStorage litemallStorage = storageService.store(file.getInputStream(), file.getSize(), file.getContentType(), originalFilename);
+        return ResultUtil.ok(litemallStorage);
+    }
+
+    /**
+     * 访问存储对象
+     *
+     * @param key 存储对象key
+     * @return
+     */
+    @GetMapping("/fetch/{key:.+}")
+    public ResponseEntity<Resource> fetch(@PathVariable String key, HttpServletRequest request) {
+        LitemallStorage litemallStorage = litemallStorageService.findByKey(key);
+        if (key == null) {
+            return ResponseEntity.notFound().build();
+        }
+        if (key.contains("../")) {
+            return ResponseEntity.badRequest().build();
+        }
+        String type = litemallStorage.getType();
+        MediaType mediaType = MediaType.parseMediaType(type);//先查数据库 拿到文件类型
+
+        Resource file = storageService.loadAsResource(key);//在从存储系统中取到他
+        if (file == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().contentType(mediaType).body(file);
+    }
+
+    /**
+     * 访问存储对象
+     *
+     * @param key 存储对象key
+     * @return
+     */
+    @GetMapping("/download/{key:.+}")
+    public ResponseEntity<Resource> download(@PathVariable String key) {
+        LitemallStorage litemallStorage = litemallStorageService.findByKey(key);
+        if (key == null) {
+            return ResponseEntity.notFound().build();
+        }
+        if (key.contains("../")) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        String type = litemallStorage.getType();
+        MediaType mediaType = MediaType.parseMediaType(type);
+
+        Resource file = storageService.loadAsResource(key);
+        if (file == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().contentType(mediaType).header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+}
